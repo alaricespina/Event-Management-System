e2ff42d189a37367f3f7345b01a1d2e75f1dc478
alaricespina <alaricsespina@gmail.com>
2024-03-02 20:02:49 +0800

    View Schedule Updated
    Removed viewEvent, View Schedule should suffice
    Creation of Dummy Events changed to check multiple instances per viewing of schedule

--- EventOrganizer.java
+++ EventOrganizer.java
@@ -8,6 +8,7 @@ public class EventOrganizer {
     static Scanner scan = new Scanner(System.in);
 
 
+
     public static void main(String[] args) {
         MenuHelper MH = new MenuHelper();
 
@@ -55,6 +56,7 @@ public class EventOrganizer {
         ValidDates.put(10, 31);
         ValidDates.put(11, 30);
         ValidDates.put(12, 31);
+
         
     }
     
@@ -81,7 +83,14 @@ public class EventOrganizer {
     }
 
     public static void createDummyEvents() {
-        SimpleCalendar[] scs = {new SimpleCalendar(2024, 8, 28), new SimpleCalendar(1024, 1, 15), new SimpleCalendar(0024, 3, 22)};
+        SimpleCalendar[] scs = {new SimpleCalendar(2024, 8, 28), 
+            new SimpleCalendar(2024, 1, 28),
+            new SimpleCalendar(2024, 2, 28),
+            new SimpleCalendar(1024, 1, 15), 
+            new SimpleCalendar(1024, 1, 12), 
+            new SimpleCalendar(1024, 1, 13), 
+            new SimpleCalendar(0024, 3, 22)
+        };
         Event e = new Event(100, "Birthday", "Mansion");
         
         for (SimpleCalendar sc : scs) {
@@ -186,10 +195,6 @@ public class EventOrganizer {
 
     }
 
-    public static void viewEvent() {
-
-    }
-
     public static void viewSchedule() {
         System.out.println("Recurring Event Schedule:");
         ArrayList<SimpleCalendar> resKs = new ArrayList<>(RecurringEventStorage.keySet());
@@ -203,12 +208,152 @@ public class EventOrganizer {
         Collections.sort(sesKs);
         System.out.println(sesKs);
 
-        int continueViewing = expectInt(new String[] {"View Schedule of specific dates?", "[0] - No", "[1] - Yes"}, null, 0, 1);
+        int continueViewing = expectInt(new String[] {"View Schedule?", "[0] - Return ", "[1] - Show Yearly Schedule", "[2] - Show Monthly Schedule"}, null, 0, 2);
+        
+        // Yearly Schedule Option
+        if (continueViewing == 1) {
 
-        if (continueViewing == 1)
-        for (SimpleCalendar sc : resKs) {
-            System.out.println(sc);
+            HashMap<Integer, ArrayList<Event>> yearly_storage = new HashMap<>();
+            HashMap<Integer, ArrayList<Event>> normal_yearly_storage = new HashMap<>();
+
+            for (SimpleCalendar sc : resKs) {
+                if (!yearly_storage.containsKey(sc.Year)) {
+                    yearly_storage.put(sc.Year, new ArrayList<>());
+                }
+
+                ArrayList<Event> currentList = RecurringEventStorage.get(sc);
+                ArrayList<Event> iterationList = yearly_storage.get(sc.Year);
+                iterationList.addAll(currentList);
+                yearly_storage.put(sc.Year, iterationList);
+            }
+
+            for (SimpleCalendar sc : sesKs) {
+                if (!normal_yearly_storage.containsKey(sc.Year)) {
+                    normal_yearly_storage.put(sc.Year, new ArrayList<>());
+                }
+
+                ArrayList<Event> currentList = SingleEventStorage.get(sc);
+                ArrayList<Event> iterationList = normal_yearly_storage.get(sc.Year);
+                iterationList.addAll(currentList);
+                normal_yearly_storage.put(sc.Year, iterationList);
+            }
+            
+            System.out.println("Recurring Yearly Schedule Summary:");
+            for (Integer year : yearly_storage.keySet()) {
+                System.out.printf("Year : %d | Num Events: %d", year, yearly_storage.get(year).size());
+                System.out.println();
+            }
+
+            System.out.println("Single Occurence Yearly Schedule Summary:");
+            for (Integer year: normal_yearly_storage.keySet()) {
+                System.out.printf("Year : %d | Num Events: %d", year, normal_yearly_storage.get(year).size());
+                System.out.println();
+            }
+
+            int proceed_details = expectInt(new String[] {"Proceed with viewing each event details?", "[0] - No", "[1] - Yes"}, null, 0, 1);
+
+            if (proceed_details == 1) {
+                System.out.println("Recurring Yearly Event Details:");
+                for ( Integer year : yearly_storage.keySet()) {
+                    ArrayList<Event> currentList = yearly_storage.get(year);
+                    System.out.printf("Year : %d | Num Events: %d", year, currentList.size());
+                    System.out.println();
+
+                    for (Event e : currentList) {
+                        System.out.println(e);
+                    }
+                }
+
+                System.out.println("Single Occurence Yearly Event Details:");
+                for ( Integer year : normal_yearly_storage.keySet()) {
+                    ArrayList<Event> currentList = normal_yearly_storage.get(year);
+                    System.out.printf("Year : %d | Num Events: %d", year, currentList.size());
+                    System.out.println();
+
+                    for (Event e : currentList) {
+                        System.out.println(e);
+                    }
+                }
+
+
+            }
+
+
+
+        // Monthly Schedule Option
+        } else if (continueViewing == 2) {
+
+                HashMap<String, ArrayList<Event>> monthly_storage = new HashMap<>();
+                HashMap<String, ArrayList<Event>> normal_monthly_storage = new HashMap<>();
+                
+                for (SimpleCalendar sc : resKs) {
+                    String month_name = ValidMonths.get(sc.Month);
+
+                    if (!monthly_storage.containsKey(month_name)) {
+                        monthly_storage.put(month_name, new ArrayList<>());
+                    }
+
+                    ArrayList<Event> currentList = RecurringEventStorage.get(sc);
+                    ArrayList<Event> iterationList = monthly_storage.get(month_name);
+                    iterationList.addAll(currentList);
+                    monthly_storage.put(month_name, iterationList);
+                }
+
+                for (SimpleCalendar sc : sesKs) {
+                    String month_name = ValidMonths.get(sc.Month);
+
+                    if (!normal_monthly_storage.containsKey(month_name)) {
+                        normal_monthly_storage.put(month_name, new ArrayList<>());
+                    }
+
+                    ArrayList<Event> currentList = SingleEventStorage.get(sc);
+                    ArrayList<Event> iterationList = normal_monthly_storage.get(month_name);
+                    iterationList.addAll(currentList);
+                    normal_monthly_storage.put(month_name, iterationList);
+                }
+
+                System.out.println("Recurring Monthly Schedule Summary:");
+                for (String month : monthly_storage.keySet()) {
+                    System.out.printf("Month : %s | Num Events: %d", month, monthly_storage.get(month).size());
+                    System.out.println();
+                }
+
+                System.out.println("Single Monthly Schedule Summary:");
+                for (String month : normal_monthly_storage.keySet()) {
+                    System.out.printf("Month : %s | Num Events: %d", month, normal_monthly_storage.get(month).size());
+                    System.out.println();
+                }
+
+                int proceed_details = expectInt(new String[] {"Proceed with viewing each event details?", "[0] - No", "[1] - Yes"}, null, 0, 1);
+
+                if (proceed_details == 1) {
+                    System.out.println("Recurring Monthly Event Details:");
+                    for (String month : monthly_storage.keySet()) {
+                        ArrayList<Event> currentList = monthly_storage.get(month);
+                        System.out.printf("Month : %s | Num Events: %d", month, currentList.size());
+                        System.out.println();
+
+                        for (Event e : currentList) {
+                            System.out.println(e);
+                        }
+                    }
+
+                    System.out.println("Single Occurence Monthly Event Details:");
+                    for (String month : normal_monthly_storage.keySet()) {
+                        ArrayList<Event> currentList = normal_monthly_storage.get(month);
+                        System.out.printf("Month : %s | Num Events: %d", month, currentList.size());
+                        System.out.println();
+
+                        for (Event e : currentList) {
+                            System.out.println(e);
+                        }
+                    }
+
+            } else {
+                return;
+            }
         }
+
     }
 
     public static void modifyEvent() {
